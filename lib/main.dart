56d902d72e88f61f4cbe6a163faa383fa778d7ba
MuhammadHafid <hafidazis385@gmail.com>
2023-03-16 00:40:08 +0700

    rename file detail_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:newflutter2/detail.screen.dart';
+import 'package:newflutter2/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
